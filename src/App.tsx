f01f48951f55ae342abfb2415fd038d5426d12ff
Zachary Heil <210567250+PixelforgeIT-UTP@users.noreply.github.com>
2025-05-07 23:28:27 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "mapbox-gl/dist/mapbox-gl.css";
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
+
 
 
 
